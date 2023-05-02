d36a8c3923949b03e9d30252a536c5c95c4215f5
GimmickNG <GimmickNG@users.noreply.github.com>
2023-05-01 22:47:35 -0600

    fix async serial host example

--- examples/async_rtu_host_example.py
+++ examples/async_rtu_host_example.py
@@ -22,25 +22,31 @@ except ImportError:
     import asyncio
 
 from umodbus.asynchronous.serial import AsyncSerial as ModbusRTUMaster
+from examples.common.register_definitions import register_definitions
 from examples.common.rtu_host_common import IS_DOCKER_MICROPYTHON
-from examples.common.rtu_host_common import register_definitions
 from examples.common.rtu_host_common import slave_addr, uart_id
 from examples.common.rtu_host_common import baudrate, rtu_pins, exit
 from examples.common.host_tests import run_async_host_tests
 
 
-async def start_rtu_host(unit_id,
-                         pins,
-                         baudrate,
-                         uart_id,
-                         **kwargs):
+async def start_rtu_host(rtu_pins,
+                         baudrate=9600,
+                         data_bits=8,
+                         stop_bits=1,
+                         parity=None,
+                         ctrl_pin=12,
+                         uart_id=1):
     """Creates an RTU host (client) and runs tests"""
 
-    host = ModbusRTUMaster(unit_id,
-                           pins=pins,
-                           baudrate=baudrate,
-                           uart_id=uart_id,
-                           **kwargs)
+    host = ModbusRTUMaster(
+        pins=rtu_pins,          # given as tuple (TX, RX)
+        baudrate=baudrate,      # optional, default 9600
+        data_bits=data_bits,    # optional, default 8
+        stop_bits=stop_bits,    # optional, default 1
+        parity=parity,          # optional, default None
+        ctrl_pin=ctrl_pin,      # optional, control DE/RE
+        uart_id=uart_id         # optional, default 1, see port specific docs
+    )
 
     print('Requesting and updating data on RTU client at address {} with {} baud'.
           format(slave_addr, baudrate))
@@ -51,12 +57,11 @@ async def start_rtu_host(unit_id,
         assert host._uart._is_server is False
 
     await run_async_host_tests(host=host,
-                               slave_addr=unit_id,
+                               slave_addr=slave_addr,
                                register_definitions=register_definitions)
 
 # create and run task
 task = start_rtu_host(
-    unit_id=slave_addr,
     pins=rtu_pins,          # given as tuple (TX, RX)
     baudrate=baudrate,      # optional, default 9600
     # data_bits=8,          # optional, default 8
